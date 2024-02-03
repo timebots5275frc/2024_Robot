099119b35638868f4872a4b8050a4747f8da1b2e
BEESECHURGERISGOD <adrianmoen2@gmail.com>
2024-02-03 09:11:42 -0600

    sillymath fix

--- src/main/java/frc/robot/CustomTypes/Math/SillyMath.java
+++ src/main/java/frc/robot/CustomTypes/Math/SillyMath.java
@@ -12,6 +12,6 @@ public class SillyMath {
     public static double goodSqrt(double in)
     {
         if (in >= 0) { return Math.sqrt(in); }
-        else { return -Math.sqrt(-in); }
+        return -Math.sqrt(-in);
     }
 }
